ff2797d5f5df42792d72e89b0c2020584f81fefb
tuanlinhtl17 <tuanlinhtl17@gmail.com>
2017-10-15 22:19:48 +0700

    update comment - not complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by(id: params[:id])
     @user = User.find_by(id: @post.user_id)
+    @comment = Comment.new
   end
 
   def new
