9cbf92161937155146c226d31ccebd2578d09b91
tuanlinhtl17 <tuanlinhtl17@gmail.com>
2017-10-20 20:54:59 +0700

    update js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
   $("#new_comment").submit(function(event){
     event.preventDefault();
 
-    let action = $(this).attr('action');
-    let method = $(this).attr('method');
+    var action = $(this).attr('action');
+    var method = $(this).attr('method');
 
-    let content = $('textarea').val();
-    let post_id = $('#comment_post_id').val();
+    var content = $('textarea').val();
+    var post_id = $('#comment_post_id').val();
 
     // $.ajax({
     //   method: method,
@@ -28,7 +28,7 @@ $(document).ready(function(){
     $.post(action + '.json', data, function(response){
       //console.log(response);
       var html = '<div class="media mb-4"><img class="d-flex mr-3 rounded-circle" src='
-      html += `${response.avatar} alt="" style="height: 50px; width: 50px"> <div class="media-body">`;
+      html += response.avatar + ' alt="" style="height: 50px; width: 50px"> <div class="media-body">';
       html += '<h5 class="mt-0">' + response.user_name + '</h5>';
       html += response.content;
       html += '</div>' + '</div>';
