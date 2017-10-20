6074afec9f69f89ad9bccf98570420f8362f6e38
tuanlinhtl17 <tuanlinhtl17@gmail.com>
2017-10-20 19:49:42 +0700

    update admin, post: update, edit, delete, user: update, edit, delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
   def show
     @post = Post.find_by(id: params[:id])
     @user = User.find_by(id: @post.user_id)
@@ -28,12 +34,46 @@ class PostsController < ApplicationController
       flash[:danger] = "You must log in first"
       redirect_to login_path
     end
-
   end
 
+  def edit
+    if logged_in?
+      @post = Post.find_by(id: params[:id])
+    else
+      flash[:danger] = "You must log in first"
+      redirect_to login_path
+    end
+  end
+
+  def update
+    if logged_in?
+      @post = Post.find_by(id: params[:id])
+      if @post.update_attributes(post_params)
+        flash[:success] = "Post updated"
+        redirect_to @post
+      else
+        render "edit"
+      end
+    else
+      flash[:danger] = "You must log in first"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    flash[:success] = "Post deleted"
+    redirect_to root_url
+  end
 
   private
-  def post_params
-    params.require(:post).permit(:content, :title, :img_url).merge(user_id: current_user.id)
-  end
+    def post_params
+      params.require(:post).permit(:content, :title, :img_url).merge(user_id: current_user.id)
+    end
+
+    def correct_user
+      @user = User.find_by(id: current_user.id)
+      @post = Post.find_by(id: params[:id])
+      redirect_to root_url unless (@post.user_id == current_user.id) || current_user.admin?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171009112001) do
+ActiveRecord::Schema.define(version: 20171020111046) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171009112001) do
     t.datetime "updated_at", null: false
     t.string "remember_digest"
     t.string "avatar_url"
+    t.boolean "admin"
   end
 
 end
