8824574d174d75938bf1641ca49d64175ddf91b4
tuanlinhtl17 <tuanlinhtl17@gmail.com>
2017-10-30 18:05:06 +0700

    change js back to es5

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     //     // $("#comments").append(html);
     //   }
     // });
-    let data = { comment: { content: content, post_id: post_id } };
+    var data = { comment: { content: content, post_id: post_id } };
 
     $.post(action + '.json', data, function(response){
       //console.log(response);
@@ -38,12 +38,12 @@ $(document).ready(function(){
       $('input[type="submit"]').prop('disabled', false);
       $('html, body').animate({ scrollTop: $(document).height()}, 'slow');
 
-    let action = $(this).attr('action');
-    let method = $(this).attr('method');
+    var action = $(this).attr('action');
+    var method = $(this).attr('method');
 
-    let content = $('textarea').val();
-    let post_id = $('#comment_post_id').val();
-    let data = { comment: { content: content, post_id: post_id } }
+    var content = $('textarea').val();
+    var post_id = $('#comment_post_id').val();
+    var data = { comment: { content: content, post_id: post_id } }
     // let JSONdata = JSON.stringify(data);
     // $.ajax({
     //   type: method,
@@ -69,7 +69,7 @@ $(document).ready(function(){
       $.post(action + '.json', data, function(response){
         console.log(response);
         var html = '<div class="media mb-4"><img class="d-flex mr-3 rounded-circle" src='
-        html += `${response.avatar} alt="" style="height: 50px; width: 50px"> <div class="media-body">`;
+        html += response.avatar + ' alt="" style="height: 50px; width: 50px"> <div class="media-body">';
         html += '<h5 class="mt-0">' + response.user_name + '</h5>';
         html += response.content;
         html += '</div>' + '</div>';
